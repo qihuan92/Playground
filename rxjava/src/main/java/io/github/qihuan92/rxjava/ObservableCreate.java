f41787a57bd7922a854f12e3ffaa3790828ff42e
qihuan <qihuan92@126.com>
2021-11-23 13:44:30 +0800

    Emitter调整

--- rxjava/src/main/java/io/github/qihuan92/rxjava/ObservableCreate.java
+++ rxjava/src/main/java/io/github/qihuan92/rxjava/ObservableCreate.java
@@ -20,4 +20,28 @@ public class ObservableCreate<T> extends Observable<T> {
         CreateEmitter<T> createEmitter = new CreateEmitter<>(observer);
         source.onSubscribe(createEmitter);
     }
+
+    static final class CreateEmitter<T> implements Emitter<T> {
+
+        private final Observer<T> observer;
+
+        public CreateEmitter(Observer<T> observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void onNext(T t) {
+            observer.onNext(t);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            observer.onError(e);
+        }
+
+        @Override
+        public void onComplete() {
+            observer.onComplete();
+        }
+    }
 }
